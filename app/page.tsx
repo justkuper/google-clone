4d12ff16bf9df47ac8a839ebd022f5b168d555a9
Kuper Bank <KuperBank@Tommi-Roses-15-MacBook-Pro-2.local>
2024-11-11 05:32:26 -0800

    added flex-center h-screen to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="center">
-      <h1 className="text-3xl font-bold text-brand">
-        StoreIt - The only storage solution you need.{" "}
-      </h1>
+    <div className="flex-center h-screen">
+      <h1 className="h1">StoreIt - The only storage solution you need. </h1>
     </div>
   );
 }
