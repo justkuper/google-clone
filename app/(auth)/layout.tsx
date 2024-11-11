f0814873d21ede11a9b9c2cfb06cc7b6c5e51cbf
Kuper Bank <KuperBank@Tommi-Roses-15-MacBook-Pro-2.local>
2024-11-11 08:23:22 -0800

    AuthForm created

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -25,10 +25,21 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
             width={342}
             height={342}
             className="transition-all hover:rotate-2 hover:scale-105"
-          ></Image>
+          />
         </div>
       </section>
-      {children}
+      <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
+        <div className="mb-16 lg:hidden">
+          <Image
+            src="/assets/icons/logo-full-brand.svg"
+            alt="logo"
+            width={224}
+            height={82}
+            className="w- h-auto w-[200px] lg:w-[250px]"
+          />
+        </div>
+        {children}
+      </section>
     </div>
   );
 };
---
+++ components/AuthForm.tsx
@@ -0,0 +1,7 @@
+type FormType = "sign-in" | "sign-up";
+
+const AuthForm = ({ type }: FormType) => {
+  return <div>AuthForm</div>;
+};
+
+export default AuthForm;
