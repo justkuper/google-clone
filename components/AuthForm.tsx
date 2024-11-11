f3c774bf3b7b9a5c1aca4a8625ccbba43645e6d9
Kuper Bank <KuperBank@Tommi-Roses-15-MacBook-Pro-2.local>
2024-11-11 08:51:15 -0800

    added username field

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,7 +1,68 @@
-type FormType = "sign-in" | "sign-up";
+"use client";
 
-const AuthForm = ({ type }: FormType) => {
-  return <div>AuthForm</div>;
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+// Define the schema using zod
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+});
+
+// Define FormType for type-safety of form props
+type FormTypeProps = {
+  type: "sign-in" | "sign-up";
+};
+
+const AuthForm = ({ type }: FormTypeProps) => {
+  // Define form with zod schema validation
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  // Define submit handler
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 };
 
 export default AuthForm;
