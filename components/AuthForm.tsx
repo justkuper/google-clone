f8dd395d5d5f26b341d7b221ae9a1830ddcfde69
Kuper Bank <KuperBank@Tommi-Roses-15-MacBook-Pro-2.local>
2024-11-11 09:56:34 -0800

    sign in and sign up buttons added to the bottom of the sign up and sign in pages

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -17,6 +17,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 // Define the schema using zod
 const formSchema = z.object({
@@ -107,6 +108,19 @@ const AuthForm = ({ type }: { FormTypeProps }) => {
           )}
         </Button>
         {errorMessage && <p className="error-message">*{errorMessage}</p>}
+        <div className="body-2 flex justify-center">
+          <p className="text-light-100">
+            {type === "sign-in"
+              ? "Don't have an account?"
+              : "Already have an account?"}
+          </p>
+          <Link
+            href={type === "sign-in" ? "/sign-up" : "/sign-in"}
+            className="ml-1 font-medium text-brand"
+          >
+            {type === "sign-in" ? "Sign Up" : "Sign In"}
+          </Link>
+        </div>
       </form>
     </Form>
   );
