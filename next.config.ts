24c2a514d15a7b98ae88d5845d76ac44a2f6e9c2
Kuper Bank <KuperBank@Tommi-Roses-15-MacBook-Pro-2.local>
2024-11-11 06:44:57 -0800

    updated next.config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,33 @@
 import type { NextConfig } from "next";
 
-// Define the Next.js configuration object
 const nextConfig: NextConfig = {
-  reactStrictMode: true, // Example option
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "100MB",
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.pixabay.com",
+      },
+      {
+        protocol: "https",
+        hostname: "img.freepik.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cloud.appwrite.io",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
