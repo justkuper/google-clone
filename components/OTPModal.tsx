04f4d2629cd72694ca369b9d07c6aebc07e1ced6
Kuper Bank <KuperBank@Tommi-Roses-15-MacBook-Pro-2.local>
2024-11-12 02:52:28 -0800

    OTPModal container edited to say enter your otp and we've sent a code to

--- components/OTPModal.tsx
+++ components/OTPModal.tsx
@@ -16,17 +16,54 @@ import {
   InputOTPSeparator,
   InputOTPSlot,
 } from "@/components/ui/input-otp";
+import React, { FormEvent, useState } from "react";
+import Image from "next/image";
 
-const OtpModal = () => {
+const OtpModal = ({
+  accountId,
+  email,
+}: {
+  accountId: string;
+  email: string;
+}) => {
+  const [isOpen, setIsOpen] = useState(true);
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      // Call API to verify OTP
+    } catch (error) {
+      console.log("Failed to verify OTP", error);
+    }
+
+    setIsLoading(false);
+  };
+
+  const handleResendOtp = async () => {
+    // Call API to resend OTP
+  };
+
   return (
-    <AlertDialog>
-      <AlertDialogTrigger>Open</AlertDialogTrigger>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-          <AlertDialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+    <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
+      <AlertDialogContent className="shad-alert-dialog">
+        <AlertDialogHeader className="relative flex justify-center">
+          <AlertDialogTitle className="h2 text-center">
+            Enter your OTP
+            <Image
+              src="/assets/icons/close-dark.svg"
+              alt="close"
+              width={20}
+              height={20}
+              onClick={() => setIsOpen(false)}
+              className="otp-close-button"
+            />
+          </AlertDialogTitle>
+          <AlertDialogDescription className="subtile-2 text-center text-light-100">
+            We've sent a code to <span>{email}</span>
           </AlertDialogDescription>
         </AlertDialogHeader>
 
